48922010c932ae5cc7a5fafa198732b56ff31619
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-02-14 21:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//your code here
+var todovalue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");
+
+function addTodo(){
+	if(todovalue.value){
+		var li = document.createElement("li");
+		li.textContent = todovalue.value;
+		ol.appendChild(li);
+		todovalue.value = "";
+	}
+}
 
